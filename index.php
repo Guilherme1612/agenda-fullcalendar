714f63355e705a041a5a515883cae8084643385b
Guilherme Avelino <guilhermeavelino1@outlook.com>
2020-04-12 23:20:49 -0300

    Cadastrando e editando eventos

--- index.php
+++ index.php
@@ -1,30 +1,38 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset='utf-8' />
+    <meta charset='utf-8' />
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-  <link href='packages/core/main.min.css' rel='stylesheet' />
-  <link href='packages/daygrid/main.min.css' rel='stylesheet' />
-  <link href='packages/timegrid/main.min.css' rel='stylesheet' />
-  <link href='packages/list/main.min.css' rel='stylesheet' />
-  <link href='css/style.css' rel='stylesheet' />
+    <link href='packages/core/main.min.css' rel='stylesheet' />
+    <link href='packages/daygrid/main.min.css' rel='stylesheet' />
+    <link href='packages/timegrid/main.min.css' rel='stylesheet' />
+    <link href='packages/list/main.min.css' rel='stylesheet' />
+    <link href='css/style.css' rel='stylesheet' />
 
-  <script src='packages/core/main.min.js'></script>
-  <script src='packages/core/locales/pt-br.js'></script>
-  <script src='packages/daygrid/main.min.js'></script>
-  <script src='packages/timegrid/main.min.js'></script>
-  <script src='packages/list/main.min.js'></script>
-  <script src='packages/interaction/main.min.js'></script>
-  <script src="js/script.js"></script>
+    <script src='packages/core/main.min.js'></script>
+    <script src='packages/core/locales/pt-br.js'></script>
+    <script src='packages/daygrid/main.min.js'></script>
+    <script src='packages/timegrid/main.min.js'></script>
+    <script src='packages/list/main.min.js'></script>
+    <script src='packages/interaction/main.min.js'></script>
+    <script src="js/script.js"></script>
 
 </head>
 <body>
-
+    <?php
+        if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        }
+    ?>
   <div id='calendar'></div>
 
   <div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -37,19 +45,73 @@
                 </button>
             </div>
             <div class="modal-body">
-                <dl class="row">
-                    <dt class="col-sm-3">ID do evento</dt>
-                    <dd class="col-sm-9" id="id"></dd>
-                    
-                    <dt class="col-sm-3">Título do evento</dt>
-                    <dd class="col-sm-9" id="title"></dd>
-                    
-                    <dt class="col-sm-3">Início do evento</dt>
-                    <dd class="col-sm-9" id="start"></dd>
-                    
-                    <dt class="col-sm-3">Fim do evento</dt>
-                    <dd class="col-sm-9" id="end"></dd>
-                </dl>
+
+                <div class="visevent">
+                    <dl class="row">
+                        <dt class="col-sm-3">ID do evento</dt>
+                        <dd class="col-sm-9" id="id"></dd>
+                        
+                        <dt class="col-sm-3">Título do evento</dt>
+                        <dd class="col-sm-9" id="title"></dd>
+                        
+                        <dt class="col-sm-3">Início do evento</dt>
+                        <dd class="col-sm-9" id="start"></dd>
+                        
+                        <dt class="col-sm-3">Fim do evento</dt>
+                        <dd class="col-sm-9" id="end"></dd>
+                    </dl>
+                    <button class="btn btn-warning btn-canc-vis">Editar</button>
+                </div>
+
+                <div class="formedit">
+                    <span id="msg-edit"></span>
+                    <form id="editevent" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="id" id="id" >
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Título</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="title" class="form-control" id="title" placeholder="Título do evento">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Color</label>
+                            <div class="col-sm-10">
+                                <select name="color" class="form-control" id="color">
+                                    <option value="">Selecione</option>			
+                                    <option style="color:#FFD700;" value="#FFD700">Amarelo</option>
+                                    <option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
+                                    <option style="color:#FF4500;" value="#FF4500">Laranja</option>
+                                    <option style="color:#8B4513;" value="#8B4513">Marrom</option>	
+                                    <option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
+                                    <option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
+                                    <option style="color:#A020F0;" value="#A020F0">Roxo</option>
+                                    <option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>
+                                    <option style="color:#228B22;" value="#228B22">Verde</option>
+                                    <option style="color:#8B0000;" value="#8B0000">Vermelho</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Início do evento</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="start" class="form-control" id="start" onkeypress="DataHora(event, this)">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Final do evento</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="end" class="form-control" id="end"  onkeypress="DataHora(event, this)">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-sm-10">
+                                <button type="button" class="btn btn-primary btn-canc-edit">Cancelar</button>
+                                <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent" class="btn btn-success">Cadastrar</button>                                    
+                            </div>
+                        </div>
+                    </form>                            
+                </div>
             </div>
         </div>
     </div>
@@ -65,7 +127,8 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <form>
+                        <span id="msg-cad"></span>
+                        <form id="addevent" action="" method="post" enctype="multipart/form-data">
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label">Título</label>
                                 <div class="col-sm-10">
@@ -105,7 +168,7 @@
 
                             <div class="form-group row">
                                 <div class="col-sm-10">
-                                    <input type="button" name="CadEvent" id="CadEvent" value="Cadastrar" class="btn btn-success">
+                                    <button type="submit" name="CadEvent" id="CadEvent" class="btn btn-success">Cadastrar</button>
                                 </div>
                             </div>
                         </form>
--- js/script.js
+++ js/script.js
@@ -19,15 +19,21 @@
               cachebuster: new Date().valueOf()
           };
       },
+
       eventClick: function (info) {
-          info.jsEvent.preventDefault(); // don't let the browser navigate
-          
-          $('#visualizar #id').text(info.event.id);
-          $('#visualizar #title').text(info.event.title);
-          $('#visualizar #start').text(info.event.start.toLocaleString());
-          $('#visualizar #end').text(info.event.end.toLocaleString());
-          $('#visualizar').modal('show');
-      },
+        info.jsEvent.preventDefault(); // don't let the browser navigate
+        console.log(info.event);
+        $('#visualizar #id').text(info.event.id);
+        $('#visualizar #id').val(info.event.id);
+        $('#visualizar #title').text(info.event.title);
+        $('#visualizar #title').val(info.event.title);
+        $('#visualizar #start').text(info.event.start.toLocaleString());
+        $('#visualizar #start').val(info.event.start.toLocaleString());
+        $('#visualizar #end').text(info.event.end.toLocaleString());
+        $('#visualizar #end').val(info.event.end.toLocaleString());
+        $('#visualizar #color').val(info.event.backgroundColor);
+        $('#visualizar').modal('show');
+    },
       selectable: true,
       select: function (info) {
           //alert('Início do evento: ' + info.start.toLocaleString());
@@ -74,3 +80,56 @@ function DataHora(evento, objeto) {
       event.returnValue = false;
   }
 }
+
+
+$(document).ready(function () {
+    $("#addevent").on("submit", function (event) {
+        event.preventDefault();
+       $.ajax({
+            method: "POST",
+            url: "cad_event.php",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (retorna) {
+                if (retorna['sit']) {
+                    //$("#msg-cad").html(retorna['msg']);
+                    location.reload();
+                } else {
+                    $("#msg-cad").html(retorna['msg']);
+                }
+            }
+        })
+    });
+    
+    $('.btn-canc-vis').on("click", function(){
+        $('.visevent').slideToggle();
+        $('.formedit').slideToggle();
+    });
+    
+    $('.btn-canc-edit').on("click", function(){
+        $('.formedit').slideToggle();
+        $('.visevent').slideToggle();
+    });
+    
+    $("#editevent").on("submit", function (event) {
+        event.preventDefault();
+       $.ajax({
+            method: "POST",
+            url: "edit_event.php",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (retorna) {
+                if (retorna['sit']) {
+                    //$("#msg-cad").html(retorna['msg']);
+                    location.reload();
+                } else {
+                    $("#msg-edit").html(retorna['msg']);
+                }
+            }
+        })
+    });
+});
+
+
